54803a7e4f3fc5c8463adb9a43726b1944b284c5
itzrhea <send2rhea@gmail.com>
2014-04-26 14:17:21 -0700

    pythonroom update

--- turtledraw.py
+++ turtledraw.py
@@ -1,1 +1,16 @@
 # author: itzrhea
+
+import turtle
+
+rhea = turtle.Turtle()
+
+length = 75
+angle = 148
+
+numbers = range(0,20)
+# rhea is cool
+#colors = ["magenta", "yellow", "black", "green"]
+
+for number in numbers:
+	rhea.forward(length)
+	rhea.left(angle)
